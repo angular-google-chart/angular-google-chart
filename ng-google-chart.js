5fcbccba2cd3ecde6fa6b2f98e3e34b1c847df0b
Nicolas Bouillon <nicolas@bouil.org>
2013-10-03 08:58:14 -0400

    Bumb version number

--- ng-google-chart.js
+++ ng-google-chart.js
@@ -1,6 +1,6 @@
 /**
  * @description Google Chart Api Directive Module for AngularJS
- * @version 0.3
+ * @version 0.0.4
  * @author Nicolas Bouillon <nicolas@bouil.org>
  * @author GitHub contributors
  * @license MIT
