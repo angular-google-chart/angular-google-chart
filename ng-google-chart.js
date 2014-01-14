2a40ca1ce98ff78a34a43af6aaba3055fb271f47
jtmkrueger <johnk@linux.com>
2014-01-14 14:54:16 -0700

    get rid of jquery

--- ng-google-chart.js
+++ ng-google-chart.js
@@ -56,8 +56,17 @@
 
                     window.google.load('visualization', apiConfig.version, settings);
                 };
-
-            jQuery.getScript(googleJsapiUrl, onLoad);
+                
+                (function(doc, type) {
+                    var firstScript = doc.getElementsByTagName(type)[0],
+                        script = doc.createElement(type);
+                    script.onreadystatechange = function () {
+                        if (this.readyState == 'complete') onLoad();
+                    }
+                    script.onload = onload;
+                    script.src = googleJsapiUrl;
+                    firstScript.parentNode.insertBefore(script)
+                }(document, 'script'));
 
             return function (fn, context) {
                 var args = Array.prototype.slice.call(arguments, 2);
@@ -150,7 +159,7 @@
                                 var dataTable;
                                 if ($scope.chart.data instanceof google.visualization.DataTable)
                                     dataTable = $scope.chart.data;
-                                else if (jQuery.isArray($scope.chart.data))
+                                else if (Array.isArray($scope.chart.data))
                                     dataTable = google.visualization.arrayToDataTable($scope.chart.data);
                                 else
                                     dataTable = new google.visualization.DataTable($scope.chart.data, 0.5);
