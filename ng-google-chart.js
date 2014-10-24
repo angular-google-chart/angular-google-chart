2847f39dbfaaef63d2f2e533811436068df262b0
Mike Crockett <you@somedomain.com>
2014-10-24 13:09:06 -0500

    Giving a better name for return params.

--- ng-google-chart.js
+++ ng-google-chart.js
@@ -197,16 +197,16 @@
                                         console.log(err);
                                     });
                                     google.visualization.events.addListener($scope.chartWrapper, 'select', function () {
-                                        var p = {selectedItems:$scope.chartWrapper.getChart().getSelection()};
+                                        var selectEventRetParams = {selectedItems:$scope.chartWrapper.getChart().getSelection()};
                                         // This is for backwards compatibility for people using 'selectedItem' that only wanted the first selection.
-                                        p['selectedItem'] = p['selectedItems'][0];
+                                        selectEventRetParams['selectedItem'] = selectEventRetParams['selectedItems'][0];
                                         $scope.$apply(function () {
                                             if ($attrs.select) {
                                                 console.log('Angular-Google-Chart: The \'select\' attribute is deprecated and will be removed in a future release.  Please use \'onSelect\'.');
-                                                $scope.select(p);
+                                                $scope.select(selectEventRetParams);
                                             }
                                             else {
-                                                $scope.onSelect(p);
+                                                $scope.onSelect(selectEventRetParams);
                                             }
                                         });
                                     });
