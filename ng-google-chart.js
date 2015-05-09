8fe82002eaa1f80c5a6264a5f23c10de6ef2c774
Nicholas Bering <balrog35@hotmail.com>
2015-05-09 15:23:58 -0400

    Code cleanup: Mostly refactoring anonymous functions into named functions to make code a little easier to read.  No functional changes.

--- ng-google-chart.js
+++ ng-google-chart.js
@@ -34,6 +34,7 @@
                 return (protocol ? protocol : '') + url;
             };
         })
+
         .factory('googleChartApiPromise', ['$rootScope', '$q', 'googleChartApiConfig', 'googleJsapiUrl', function ($rootScope, $q, apiConfig, googleJsapiUrl) {
             var apiReady = $q.defer();
             var onLoad = function () {
@@ -76,6 +77,7 @@
 
             return apiReady.promise;
         }])
+
         .directive('googleChart', ['$timeout', '$window', '$rootScope', 'googleChartApiPromise', function ($timeout, $window, $rootScope, googleChartApiPromise) {
             return {
                 restrict: 'A',
@@ -85,34 +87,9 @@
                         or type change. All other values intentionally disregarded to avoid double
                         calls to the draw function. Please avoid making changes to these objects
                         directly from this directive.*/
-                        var self = this;
-                    $scope.$watch('{ customFormatters: '+ $attrs.chart +'.customFormatters, ' +
-                    'data: ' + $attrs.chart + '.data, ' +
-                    'formatters: ' + $attrs.chart +'.formatters, ' +
-                    'options: ' + $attrs.chart +'.options, ' +
-                    'type: ' + $attrs.chart +'.type, ' +
-                    'view: ' + $attrs.chart +'.view }', function () {
-                    self.chart = $scope.$eval($attrs.chart);
-                        drawAsync();
-                    }, true); // true is for deep object equality checking
-
-                    // Redraw the chart if the window is resized
-                    var resizeHandler = $rootScope.$on('resizeMsg', function () {
-                        $timeout(function () {
-                            // Not always defined yet in IE so check
-                            if (self.chartWrapper) {
-                                drawAsync();
-                            }
-                        });
-                    });
-
-                    //Cleanup resize handler.
-                    $scope.$on('$destroy', function () {
-                        resizeHandler();
-                    });
-
-                    // Keeps old formatter configuration to compare against
-                    var oldChartFormatters = {};
+                    var self = this;
+                    var oldChartFormatters = {}, resizeHandler;
+                    init();
 
                     function applyFormat(formatType, formatClass, dataTable) {
                         var i;
@@ -128,10 +105,11 @@
                                         for (i = 0; i < self.chart.formatters[formatType][cIdx].formats.length; i++) {
                                             var data = self.chart.formatters[formatType][cIdx].formats[i];
 
-                                            if (typeof (data.fromBgColor) !== 'undefined' && typeof (data.toBgColor) !== 'undefined')
+                                            if (typeof (data.fromBgColor) !== 'undefined' && typeof (data.toBgColor) !== 'undefined') {
                                                 colorFormat.addGradientRange(data.from, data.to, data.color, data.fromBgColor, data.toBgColor);
-                                            else
+                                            } else {
                                                 colorFormat.addRange(data.from, data.to, data.color, data.bgcolor);
+                                            }
                                         }
 
                                         self.formatters[formatType].push(colorFormat);
@@ -149,88 +127,27 @@
 
                             //apply formats to dataTable
                             for (i = 0; i < self.formatters[formatType].length; i++) {
-                                if (self.chart.formatters[formatType][i].columnNum < dataTable.getNumberOfColumns())
+                                if (self.chart.formatters[formatType][i].columnNum < dataTable.getNumberOfColumns()) {
                                     self.formatters[formatType][i].format(dataTable, self.chart.formatters[formatType][i].columnNum);
+                                }
                             }
 
 
                             //Many formatters require HTML tags to display special formatting
-                            if (formatType === 'arrow' || formatType === 'bar' || formatType === 'color')
+                            if (formatType === 'arrow' || formatType === 'bar' || formatType === 'color') {
                                 self.chart.options.allowHtml = true;
+                            }
                         }
+                    }
+
+                    function cleanup(){
+                        resizeHandler();
                     }
 
                     function draw() {
                         if (!draw.triggered && (self.chart !== undefined)) {
                             draw.triggered = true;
-                            $timeout(function () {
-
-                                if (typeof (self.chartWrapper) === 'undefined') {
-                                    var chartWrapperArgs = {
-                                        chartType: self.chart.type,
-                                        dataTable: self.chart.data,
-                                        view: self.chart.view,
-                                        options: self.chart.options,
-                                        containerId: $element[0]
-                                    };
-
-                                    self.chartWrapper = new google.visualization.ChartWrapper(chartWrapperArgs);
-                                    google.visualization.events.addListener(self.chartWrapper, 'ready', function () {
-                                        self.chart.displayed = true;
-                                        $scope.$apply(function () {
-                                            $scope.$eval($attrs.onReady, { chartWrapper: self.chartWrapper });
-                                        });
-                                    });
-                                    google.visualization.events.addListener(self.chartWrapper, 'error', function (err) {
-                                        console.log("Chart not displayed due to error: " + err.message + ". Full error object follows.");
-                                        console.log(err);
-                                    });
-                                    google.visualization.events.addListener(self.chartWrapper, 'select', function () {
-                                        var selectEventRetParams = { selectedItems: self.chartWrapper.getChart().getSelection() };
-                                        // This is for backwards compatibility for people using 'selectedItem' that only wanted the first selection.
-                                        selectEventRetParams.selectedItem = selectEventRetParams.selectedItems[0];
-                                        $scope.$apply(function () {
-                                            if ($attrs.select) {
-                                                console.log('Angular-Google-Chart: The \'select\' attribute is deprecated and will be removed in a future release.  Please use \'onSelect\'.');
-                                                $scope.$eval($attrs.select, selectEventRetParams);
-                                            }
-                                            else {
-                                                $scope.$eval($attrs.onSelect, selectEventRetParams);
-                                            }
-                                        });
-                                    });
-                                }
-                                else {
-                                    self.chartWrapper.setChartType(self.chart.type);
-                                    self.chartWrapper.setDataTable(self.chart.data);
-                                    self.chartWrapper.setView(self.chart.view);
-                                    self.chartWrapper.setOptions(self.chart.options);
-                                }
-
-                                if (typeof (self.formatters) === 'undefined')
-                                    self.formatters = {};
-
-                                if (typeof (self.chart.formatters) !== 'undefined') {
-                                    applyFormat("number", google.visualization.NumberFormat, self.chartWrapper.getDataTable());
-                                    applyFormat("arrow", google.visualization.ArrowFormat, self.chartWrapper.getDataTable());
-                                    applyFormat("date", google.visualization.DateFormat, self.chartWrapper.getDataTable());
-                                    applyFormat("bar", google.visualization.BarFormat, self.chartWrapper.getDataTable());
-                                    applyFormat("color", google.visualization.ColorFormat, self.chartWrapper.getDataTable());
-                                }
-
-                                var customFormatters = self.chart.customFormatters;
-                                if (typeof (customFormatters) !== 'undefined') {
-                                    for (var name in customFormatters) {
-                                        applyFormat(name, customFormatters[name], self.chartWrapper.getDataTable());
-                                    }
-                                }
-
-                                $timeout(function () {
-                                    $scope.$eval($attrs.beforeDraw, { chartWrapper: $scope.chartWrapper });
-                                    self.chartWrapper.draw();
-                                    draw.triggered = false;
-                                });
-                            }, 0, true);
+                            $timeout(setupAndDraw, 0, true);
                         } else if (self.chart !== undefined) {
                             $timeout.cancel(draw.recallTimeout);
                             draw.recallTimeout = $timeout(draw, 10);
@@ -241,6 +158,106 @@
                         googleChartApiPromise.then(function () {
                             draw();
                         });
+                    }
+
+                    function drawChartWrapper(){
+                        $scope.$eval($attrs.beforeDraw, { chartWrapper: $scope.chartWrapper });
+                        self.chartWrapper.draw();
+                        draw.triggered = false;
+                    }
+
+                    function handleError(err) {
+                        console.log("Chart not displayed due to error: " + err.message + ". Full error object follows.");
+                        console.log(err);
+                    }
+
+                    function handleReady() {
+                        self.chart.displayed = true;
+                        $scope.$apply(function () {
+                            $scope.$eval($attrs.onReady, { chartWrapper: self.chartWrapper });
+                        });
+                    }
+
+                    function handleSelect() {
+                        var selectEventRetParams = { selectedItems: self.chartWrapper.getChart().getSelection() };
+                        // This is for backwards compatibility for people using 'selectedItem' that only wanted the first selection.
+                        selectEventRetParams.selectedItem = selectEventRetParams.selectedItems[0];
+                        $scope.$apply(function () {
+                            if ($attrs.select) {
+                                console.log('Angular-Google-Chart: The \'select\' attribute is deprecated and will be removed in a future release.  Please use \'onSelect\'.');
+                                $scope.$eval($attrs.select, selectEventRetParams);
+                            }
+                            else {
+                                $scope.$eval($attrs.onSelect, selectEventRetParams);
+                            }
+                        });
+                    }
+
+                    function init(){
+                        $scope.$watch('{ customFormatters: '+ $attrs.chart +'.customFormatters, ' +
+                        'data: ' + $attrs.chart + '.data, ' +
+                        'formatters: ' + $attrs.chart +'.formatters, ' +
+                        'options: ' + $attrs.chart +'.options, ' +
+                        'type: ' + $attrs.chart +'.type, ' +
+                        'view: ' + $attrs.chart +'.view }', function () {
+                            self.chart = $scope.$eval($attrs.chart);
+                            drawAsync();
+                        }, true); // true is for deep object equality checking
+
+                        // Redraw the chart if the window is resized
+                        resizeHandler = $rootScope.$on('resizeMsg', function () {
+                            $timeout(function () {
+                                // Not always defined yet in IE so check
+                                if (self.chartWrapper) {
+                                    drawAsync();
+                                }
+                            });
+                        });
+
+                        //Cleanup resize handler.
+                        $scope.$on('$destroy', cleanup());
+                    }
+
+                    function setupAndDraw(){
+                        if (typeof (self.chartWrapper) === 'undefined') {
+                            var chartWrapperArgs = {
+                                chartType: self.chart.type,
+                                dataTable: self.chart.data,
+                                view: self.chart.view,
+                                options: self.chart.options,
+                                containerId: $element[0]
+                            };
+
+                            self.chartWrapper = new google.visualization.ChartWrapper(chartWrapperArgs);
+                            google.visualization.events.addListener(self.chartWrapper, 'ready', handleReady);
+                            google.visualization.events.addListener(self.chartWrapper, 'error', handleError);
+                            google.visualization.events.addListener(self.chartWrapper, 'select', handleSelect);
+                        } else {
+                            self.chartWrapper.setChartType(self.chart.type);
+                            self.chartWrapper.setDataTable(self.chart.data);
+                            self.chartWrapper.setView(self.chart.view);
+                            self.chartWrapper.setOptions(self.chart.options);
+                        }
+
+                        if (typeof (self.formatters) === 'undefined')
+                            self.formatters = {};
+
+                            if (typeof (self.chart.formatters) !== 'undefined') {
+                                applyFormat("number", google.visualization.NumberFormat, self.chartWrapper.getDataTable());
+                                applyFormat("arrow", google.visualization.ArrowFormat, self.chartWrapper.getDataTable());
+                                applyFormat("date", google.visualization.DateFormat, self.chartWrapper.getDataTable());
+                                applyFormat("bar", google.visualization.BarFormat, self.chartWrapper.getDataTable());
+                                applyFormat("color", google.visualization.ColorFormat, self.chartWrapper.getDataTable());
+                            }
+
+                            var customFormatters = self.chart.customFormatters;
+                            if (typeof (customFormatters) !== 'undefined') {
+                                for (var name in customFormatters) {
+                                    applyFormat(name, customFormatters[name], self.chartWrapper.getDataTable());
+                                }
+                            }
+
+                            $timeout(drawChartWrapper);
                     }
                 }
             };
