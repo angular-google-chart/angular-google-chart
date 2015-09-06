03bbc3c422b9f83ed82c09bfa0404e262b6b31cb
Nicholas Bering <nick@whitemeadowsfarms.com>
2015-09-06 04:05:20 +0000

    feat(agcBeforeDraw): add before draw event directive
    Replaces old before-draw attribute from main google-chart directive which was just factored out in the previous commit.

---
+++ src/agcBeforeDraw.js
@@ -0,0 +1,22 @@
+/* global angular */
+(function(){
+    angular.module('googlechart')
+        .directive('agcBeforeDraw', onReadyDirective);
+        
+    function onReadyDirective(){
+        return {
+            restrict: 'A',
+            scope: false,
+            require: 'googleChart',
+            link: function(scope, element, attrs, googleChartController){
+                callback.$inject=['chartWrapper'];
+                function callback(chartWrapper){
+                    scope.$apply(function (){
+                        scope.$eval(attrs.agcBeforeDraw, {chartWrapper: chartWrapper});
+                    });
+                }
+                googleChartController.registerServiceListener('beforeDraw', callback, this);
+            }
+        };
+    }
+})();
