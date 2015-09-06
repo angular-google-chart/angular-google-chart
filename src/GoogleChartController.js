d816587ea4e2331d66043371e59f309dc86911cd
Nicholas Bering <nick@whitemeadowsfarms.com>
2015-09-06 03:44:45 +0000

    feat(GoogleChartController): move the majority of the complex logic to the service
    The GoogleChartController now handles the basic flow of watching the chart, and drawing on window resize, but delegates the actual wrapping of the Google Charts API to the new service.

--- src/GoogleChartController.js
+++ src/GoogleChartController.js
@@ -5,17 +5,14 @@
     angular.module('googlechart')
         .controller('GoogleChartController', GoogleChartController);
 
-    GoogleChartController.$inject = ['$scope', '$element', '$attrs', '$injector', 'FormatManager', '$timeout', '$window', '$rootScope', 'googleChartApiPromise'];
+    GoogleChartController.$inject = ['$scope', '$element', '$attrs', '$injector', '$timeout', '$window', '$rootScope', 'GoogleChartService'];
 
-    function GoogleChartController($scope, $element, $attrs, $injector, FormatManager, $timeout, $window, $rootScope, googleChartApiPromise) {
+    function GoogleChartController($scope, $element, $attrs, $injector, $timeout, $window, $rootScope, GoogleChartService) {
         var self = this;
-        var $google;
-        var resizeHandler, wrapperListeners = {},
-            formatManager,
-            chartListeners = {},
-            innerVisualization = null;
-        self.registerChartListener = registerChartListener;
-        self.registerWrapperListener = registerWrapperListener;
+        var resizeHandler;
+        self.registerChartListener = GoogleChartService.registerChartListener;
+        self.registerWrapperListener = GoogleChartService.registerWrapperListener;
+        self.registerServiceListener = GoogleChartService.registerServiceListener;
 
         init();
 
@@ -36,35 +33,14 @@
 
         // Watch function calls this.
         function drawAsync() {
-            googleChartApiPromise.then(function(g) {
-                $google = g;
-                draw();
-            });
+            GoogleChartService.getReadyPromise()
+                .then(draw);
         }
 
         //setupAndDraw() calls this.
         function drawChartWrapper() {
-            $scope.$eval($attrs.beforeDraw, {
-                chartWrapper: $scope.chartWrapper
-            });
-            self.chartWrapper.draw();
+            GoogleChartService.draw();
             draw.triggered = false;
-        }
-
-        handleError.$inject = ['args'];
-
-        function handleError(args) {
-            var error = args[0];
-            console.log("Chart not displayed due to error: '" + error.message + "' Full error object follows.");
-            console.log(error);
-        }
-
-        function handleReady() {
-            self.chart.displayed = true;
-            if (innerVisualization !== self.chartWrapper.getChart()) {
-                innerVisualization = self.chartWrapper.getChart();
-                registerListenersWithGoogle(innerVisualization, chartListeners);
-            }
         }
 
         function init() {
@@ -75,50 +51,20 @@
             $scope.$watch(watchValue, watchHandler, true); // true is for deep object equality checking
 
             // Redraw the chart if the window is resized
-            resizeHandler = $rootScope.$on('resizeMsg', function() {
-                $timeout(function() {
-                    // Not always defined yet in IE so check
-                    if (self.chartWrapper) {
-                        drawAsync();
-                    }
-                });
-            });
+            resizeHandler = $rootScope.$on('resizeMsg', drawAsync);
 
             //Cleanup resize handler.
             $scope.$on('$destroy', cleanup);
-
-            registerWrapperListener('error', handleError, self);
-            registerWrapperListener('ready', handleReady, self);
         }
 
         function setupAndDraw() {
-            if (typeof(self.chartWrapper) === 'undefined') {
-                var chartWrapperArgs = {
-                    chartType: self.chart.type,
-                    dataTable: self.chart.data,
-                    view: self.chart.view,
-                    options: self.chart.options,
-                    containerId: $element[0]
-                };
-
-                self.chartWrapper = new $google.visualization.ChartWrapper(chartWrapperArgs);
-                registerListenersWithGoogle(self.chartWrapper, wrapperListeners);
-            }
-            else {
-                self.chartWrapper.setChartType(self.chart.type);
-                self.chartWrapper.setDataTable(self.chart.data);
-                self.chartWrapper.setView(self.chart.view);
-                self.chartWrapper.setOptions(self.chart.options);
-            }
-
-            if (!formatManager) {
-                formatManager = new FormatManager($google);
-            }
-
-            if (formatManager.applyFormats(self.chartWrapper.getDataTable(),
-                    self.chart.formatters, self.chart.customFormatters).requiresHtml) {
-                self.chartWrapper.setOption('allowHtml', true);
-            }
+            GoogleChartService.setup($element,
+            self.chart.type,
+            self.chart.data,
+            self.chart.view,
+            self.chart.options,
+            self.chart.formatters,
+            self.chart.customFormatters);
 
             $timeout(drawChartWrapper);
         }
@@ -140,62 +86,6 @@
                     view: chartObject.view
                 };
             }
-        }
-
-        // This function was written to genericize listener registration
-        // because I plan to implement different collections of listeners
-        // for events on the underlying chart object, and for
-        // directive-level events (ie. beforeDraw).
-        function registerListener(listenerCollection, eventName, listenerFn, listenerObject) {
-            // This is the function that will be invoked by the charts API.
-            // Passing the wrapper function allows the use of DI for
-            // for the called function.
-            var listenerWrapper = function() {
-                var locals = {
-                    chartWrapper: self.chartWrapper,
-                    chart: self.chartWrapper.getChart(),
-                    args: arguments
-                };
-                $injector.invoke(listenerFn, listenerObject || this, locals);
-            };
-
-            if (angular.isDefined(listenerCollection) && angular.isObject(listenerCollection)) {
-                if (!angular.isArray(listenerCollection[eventName])) {
-                    listenerCollection[eventName] = [];
-                }
-                listenerCollection[eventName].push(listenerWrapper);
-                return function() {
-                    if (angular.isDefined(listenerWrapper.googleListenerHandle)) {
-                        $google.visualization.events.removeListener(listenerWrapper.googleListenerHandle);
-                    }
-                    var fnIndex = listenerCollection[eventName].indexOf(listenerWrapper);
-                    listenerCollection[eventName].splice(fnIndex, 1);
-                    if (listenerCollection[eventName].length === 0) {
-                        listenerCollection[eventName] = undefined;
-                    }
-                };
-            }
-        }
-
-        function registerListenersWithGoogle(eventSource, listenerCollection) {
-            for (var eventName in listenerCollection) {
-                if (listenerCollection.hasOwnProperty(eventName) && angular.isArray(listenerCollection[eventName])) {
-                    for (var fnIterator = 0; fnIterator < listenerCollection[eventName].length; fnIterator++) {
-                        if (angular.isFunction(listenerCollection[eventName][fnIterator])) {
-                            listenerCollection[eventName][fnIterator].googleListenerHandle =
-                                $google.visualization.events.addListener(eventSource, eventName, listenerCollection[eventName][fnIterator]);
-                        }
-                    }
-                }
-            }
-        }
-
-        function registerChartListener(eventName, listenerFn, listenerObject) {
-            return registerListener(chartListeners, eventName, listenerFn, listenerObject);
-        }
-
-        function registerWrapperListener(eventName, listenerFn, listenerObject) {
-            return registerListener(wrapperListeners, eventName, listenerFn, listenerObject);
         }
     }
 })();
