6ac90afd53fc165cab94351982f4984c50e52d08
Nicholas Bering <bering.nicholas@gmail.com>
2017-04-15 19:11:35 -0400

    [fix] Fix behaviour of library loader when loader strategy does not exist.

--- src/agcLibraryLoader.js
+++ src/agcLibraryLoader.js
@@ -7,22 +7,26 @@
 
     function AgcLibraryLoaderProvider($injector){
 
+        var DEFAULT_LOADER = "Jsapi";
+
         this.$get = function(loader){
             return loader;
         }
 
         this.setLoader = function(loaderName){
-            loaderName = loaderName.charAt(0).toUpperCase() + loaderName.slice(1);
             if ($injector.has(this.getProviderName(loaderName)))
                 this.$get.$inject = [this.getProviderName(loaderName)];
-            else
-                console.warn("AGC loader type doesn't exist. Defaulting to JSAPI.");
+            else {
+                console.warn("[Angular-GoogleChart] Loader type \"" + loaderName + "\" doesn't exist. Defaulting to JSAPI loader.");
+                this.$get.$inject = [this.getProviderName(DEFAULT_LOADER)];
+            }
         }
 
         this.getProviderName = function(loaderName){
+            loaderName = loaderName.charAt(0).toUpperCase() + loaderName.slice(1);
             return "agc" + loaderName + "Loader";
         }
 
-        this.setLoader("Jsapi");
+        this.setLoader(DEFAULT_LOADER);
     }
 })();
